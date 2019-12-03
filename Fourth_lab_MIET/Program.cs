e3a6611b7811f11d5ddf0792c3e2b39bf217f75a
sergeyampo <dev@sergeyampo.ru>
2019-12-03 17:44:09 +0300

    Add project files.

---
+++ Fourth_lab_MIET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fourth_lab_MIET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
