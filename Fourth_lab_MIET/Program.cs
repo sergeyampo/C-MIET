e53a1f363b26a2c64073eb29c43afa1cbed82280
sergeyampo <dev@sergeyampo.ru>
2019-12-06 19:20:27 +0300

    Ready to fight

--- Fourth_lab_MIET/Program.cs
+++ Fourth_lab_MIET/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
 
 namespace Fourth_lab_MIET
 {
-    class Program
+    enum Action
+    {
+        Add,
+        Remove,
+        Property
+    };
+
+    class Programm
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int потому что нам нужно зарегистрировать изменения значения группы
+            //тип которого int => KeySelector должен быть int.
+            var first = new StudentCollection<int>((e) => e.Id, "first");
+            var second = new StudentCollection<int>((e) => e.Id, "second");
+
+
+            var j = new Journal<int>();
+            j.Subscribe(first);
+            j.Subscribe(second);
+
+
+            var s1 = new Student(Education.Bachelor, 5);
+            var s2 = new Student(Education.Bachelor, 5);
+            first.AddStudents(s1);
+            second.AddStudents(s2);
+            s1.Educ = Education.SecondEducation;
+            s2.Educ = Education.Specialist;
+
+            s1.Group = 4;
+            s2.Group = 1;
+
+            first.Remove(s1.Id);
+            second.Remove(s2.Id);
+
+            s1.Educ = Education.Specialist;
+            s2.Educ = Education.Bachelor;
+
+            s1.Group = 4;
+            s2.Group = 1;
+
+            // After deleting from Collections changing not write in journal
+            Console.WriteLine(j.ToString());
         }
     }
 }
---
+++ Fourth_lab_MIET/Student.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Fourth_lab_MIET
+{
+    //
+    class StudentsChangedEventArgs<TKey> : EventArgs
+    {
+        public string NameCollection { get; set; }
+        public Action Info { get; set; }
+        public string PropertyName { get; set; }
+        public TKey Key { get; set; }
+
+        public StudentsChangedEventArgs(string name, Action act, string prop, TKey key)
+        {
+            NameCollection = name;
+            Info = act;
+            PropertyName = prop;
+            Key = key;
+        }
+
+        public override string ToString()
+        {
+            return NameCollection + " " + PropertyName;
+        }
+    };
+    enum Education
+    {
+        Specialist,
+        Bachelor,
+        SecondEducation
+    };
+
+    class Student : INotifyPropertyChanged
+    {
+        delegate TKey KeySelector<TKey>(Student st);
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Student(Education educ, int group)
+        {
+            m_educ = educ;
+            m_group = group;
+            Id = Student._id++;
+        }
+
+        public Student()
+        {
+            m_educ = Education.Specialist;
+            m_group = 1;
+            Id = Student._id++;
+        }
+
+        public Education Educ
+        {
+            get
+            {
+                return m_educ;
+            }
+
+            set
+            {
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("Education"));
+                }
+                m_educ = value;
+            }
+        }
+
+        public int Group
+        {
+            get
+            {
+                return m_group;
+            }
+
+            set
+            {
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("Group"));
+                }
+                m_group = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "" + Group;
+        }
+
+        private static int _id = 0;
+
+        public int Id;
+        private Education m_educ;
+        private int m_group;
+    };
+}
+
